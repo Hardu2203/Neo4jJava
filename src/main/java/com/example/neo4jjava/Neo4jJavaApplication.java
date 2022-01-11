5773070ef2d9cc0ef80eb0e718352c370e156b5d
hardu <hardunelhn@gmail.com>
2022-01-11 11:21:26 +0200

    initial commit

---
+++ src/main/java/com/example/neo4jjava/Neo4jJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo4jjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jJavaApplication.class, args);
+    }
+
+}
